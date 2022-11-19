90c4bfdbff230b8681a3cb7bae4c68430725945a
Nicolas Jandre <nicolasjandre@live.com>
2022-11-19 01:56:53 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const projectWrapper = document.querySelector('.projects-wrapper')
+const arrowPrev = document.querySelector('.arrow.prev')
+const arrowNext = document.querySelector('.arrow.next')
+const projectBounding = projectWrapper.getBoundingClientRect()
+const imageAndLink = projectWrapper.querySelectorAll('img, a')
+const widthToScroll = projectWrapper.children[0].offsetWidth
+let clicked = false
+let initPosition = 0
+let currentScroll = 0
+
+arrowPrev.onclick = function() {
+    projectWrapper.scrollLeft -= widthToScroll
+}
+
+arrowNext.onclick = function() {
+    projectWrapper.scrollLeft += widthToScroll
+}
+
+imageAndLink.forEach(item=> {
+    item.setAttribute('draggable', false)
+})
+
+projectWrapper.onmousedown = function (e) {
+    projectWrapper.classList.add('grab')
+    initPosition = e.clientX - projectBounding.left
+    currentScroll = projectWrapper.scrollLeft
+    clicked = true
+}
+
+projectWrapper.onmousemove = function (e) {
+    if (clicked) {
+        const xPosition = e.clientX - projectBounding.left
+        projectWrapper.scrollLeft = currentScroll + -(xPosition - initPosition)
+    }
+}
+
+function mouseUpAndLeave() {
+    projectWrapper.classList.remove('grab')
+    clicked = false
+}
+
+projectWrapper.onmouseup = mouseUpAndLeave
+projectWrapper.onmouseleave = mouseUpAndLeave
